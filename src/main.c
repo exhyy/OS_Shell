074c9b6202d28d8acdec3916fb403a5531191b99
exhyy <starryhyy@gmail.com>
2022-09-12 00:27:03 +0800

    update

--- src/main.c
+++ src/main.c
@@ -9,17 +9,6 @@ int main()
 {
     while(1)
     {
-        // int t;
-        // if (t = input_command())
-        // {
-        //     printf("错误代码：%d\n", t);
-        //     continue;
-        // }
         run_command(input_command());
-        // printf(">>> %s\n", command_buffer);
-        // // printf("commands_length=%d", commands_length);
-        // for (int i = 0; i < commands_length; i++)
-        //     printf("*** %s\n", commands[i]);
-        
     }
 }
